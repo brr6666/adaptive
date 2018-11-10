96f0b10d52816a6f722c646ff898c5598dff909c
bRR <rrb666@mail.ru>
2018-11-10 23:12:07 +0300

    Компилятор

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+    
+	
+	
+	gulp.task("style", function(){
+        gulp.src("sass/styles.scss")
+			.pipe(plumber())
+            .pipe(sass())
+			.pipe(postcss([autoprefixer]))
+            .pipe(gulp.dest("css"))
+			.pipe(server.stream());
+    });
+
+	gulp.task("server", ["style"], function(){
+		server.init({
+			server: true,
+			notify: false,
+			open: true,
+			cors: true,
+			ui: {
+				port: 8000
+			}
+		});
+	
+	gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+	gulp.watch("*.html", server.reload);
+	});
+	
+	gulp.task('default', ['server']);
